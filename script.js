5f7132310f8c4baa2883cf7505397a2f04340231
s-gurjot <singh.gurjot0509@gmail.com>
2021-08-29 13:25:39 +0530

    Version-2

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ let t=document.getElementById('title');
 let prev=document.getElementById('prev');
 let next=document.getElementById('next');
 
+let progress=document.getElementById('progress');
+
+let total_duration =document.getElementById('duration');
+let current_time =document.getElementById('current_time');
+
+const progress_div=document.getElementById('progress_div');
 //Creating Array of Objects
 let songs=[
     {
@@ -22,7 +28,12 @@ let songs=[
     {
         name:"Song-3",
         title:"Baarish Ban Jana",
-        artist:"Stebin Ben"
+        artist:"Stebin Ben & Payal Dev"
+    },
+    {
+        name:"Song-4",
+        title:"Bachalo",
+        artist:"Akhil"
     }
 ]
 
@@ -84,11 +95,83 @@ let prevSong =()=>
     playMusic();
 }
 
+//Progress JS work
+
+music.addEventListener("timeupdate", (e)=>
+{
+    //console.log(e);
+   const{currentTime, duration} = e.target;// instead of event.srcElement
+   let progress_time =(currentTime/duration)*100;
+   
+   progress.style.width=`${progress_time}%`;
+
+   //Music duration Update
+   
+   let min_duration =Math.floor(duration / 60);
+   let sec_duration =Math.floor(duration % 60);
+
+   let tot_duration=`${min_duration}:${sec_duration}`;
+
+  
+   if(duration)
+   {
+       //When duration get's calculated only then update the time
+     total_duration.textContent=`${tot_duration}`;
+   }
+
+   //Current duration Update
+   
+   let current_min =Math.floor(currentTime/60);
+   let current_sec =Math.floor(currentTime%60);
+
+   if(current_sec<10)
+   {
+       current_sec=`0${current_sec}`;
+   }
+
+     let tot_currentTime=`${current_min}:${current_sec}`; 
+     current_time.textContent=`${tot_currentTime}`;
+     
+});
+
+//progress onclick functionality
+
+progress_div.addEventListener("click",(e)=>
+{
+    //console.log(e);
+    const{duration}=music;
+    //converting % to sec, as currentTime expects time in sec
+    
+    let move_progress=(e.offsetX / progress_div.clientWidth) * duration;
+    console.log(duration);
+    console.log(move_progress);
+
+    //Giving the new time to current time property
+    music.currentTime=move_progress;
+})
+
+//If music end call next song Function
+music.addEventListener("ended",nextSong);
+
 next.addEventListener('click',nextSong);
 prev.addEventListener('click',prevSong);
 
 
-if(music.ended)
+
+
+/*
+document.addEventListener('click', (s)=>
 {
-    img.classList.remove('anime');
+    // s will always be an object of type of the event
+    console.log(s);
 }
+);
+*/
+
+// Music Duration Update
+
+     /*      ALTERNATE WAY TO DO
+        const{target}=event
+        console.log(target.currentTime);
+        console.log(target.duration);
+   */
